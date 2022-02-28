42f916d1b22acb958588b44235e37bc8585ca20d
davidkuc <davidkuc2@onet.pl>
2022-02-28 12:45:22 +0100

    Finished SupplyComm.cs

--- RestaurantApp/Entities/Supply.cs
+++ RestaurantApp/Entities/Supply.cs
@@ -10,8 +10,8 @@ namespace RestaurantApp.Entities
     public class Supply : EntityBase
     {
         public string Name { get; set; }
-        public DateOnly? ExpirationDate { get; set; }
-        public DateOnly? PurchaseDate { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+        public DateTime? PurchaseDate { get; set; }
         public string? Category { get; set; }
         public int? Quantity { get; set; }     
         public int SupplierId { get; set; }
