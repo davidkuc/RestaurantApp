44325e997a9fefefc93404b614fe253f9187e663
davidkuc <davidkuc2@onet.pl>
2022-03-01 09:05:05 +0100

    On OrderComm

--- RestaurantApp/Entities/Dish.cs
+++ RestaurantApp/Entities/Dish.cs
@@ -17,7 +17,7 @@ namespace RestaurantApp.Entities
             sb.AppendLine();
             sb.AppendLine($" {this.GetType().Name}");
             sb.AppendLine($" {Name} ID: {Id}");
-            sb.AppendLine($" PricePerDish: {Price}");
+            sb.AppendLine($" Price: {Price}");
             sb.AppendLine();
 
             return sb.ToString();
